fef964acdaad4b42806be06969e17c302950402a
Karim Elhawaty <kaelhawaty@gmail.com>
2021-05-06 20:45:53 +0200

    Added release executable

--- main.cpp
+++ main.cpp
@@ -4,10 +4,27 @@
 #include "src/Parser/LexicalParser.h"
 
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::string inputPath = "..\\input";
-    Parser p;
-    p.parse(inputPath);
+    if(argc != 3){
+        std::cout << "Error: You need to specify both the rules file path and program file path." << "\n";
+        std::cout << "Usage: " << argv[0] << " rulesFilePath programFilePath" << "\n";
+        return 0;
+    }
+    std::string rulesPath{argv[1]};
+    LexicalParser lexicalParser(rulesPath);
+    std::string programPath{argv[2]};
+    lexicalParser.set_input_stream(programPath);
+    Token token;
+    std::ofstream outputFile{"output.txt"};
+    if(!outputFile.is_open()){
+        std::cout << "Error: Couldn't create output file." << "\n";
+        return 0;
+    }
+    while(lexicalParser.get_next_token(token)){
+        outputFile << token.regEXP << "\n";
+    }
+    outputFile.flush();
+    outputFile.close();
     return 0;
 }
