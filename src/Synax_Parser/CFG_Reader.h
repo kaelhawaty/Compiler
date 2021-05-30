6cbc4d9e3fc08c887d8a152d98a9a7bdbb4b0ea4
Hazem Shawky <hazemahmed575@gmail.com>
2021-05-30 19:53:57 +0200

    added CFG_Reader

--- src/Synax_Parser/CFG_Reader.h
+++ src/Synax_Parser/CFG_Reader.h
@@ -4,19 +4,33 @@
 
 #include <unordered_map>
 #include <vector>
+#include <fstream>
+#include <iostream>
+#include <cassert>
+#include "../Parser/Utils/ParserUtils.h"
 #ifndef COMPILER_CFG_READER_H
 #define COMPILER_CFG_READER_H
 
 #endif //COMPILER_CFG_READER_H
+
 enum class Type {
     TERMINAL,
     NON_TERMINAL,
     EPSILON
 };
+
 struct Symbol {
     std::string name;
     Type type;
 };
+
 class CFG_Reader {
+public:
     std::unordered_map<std::string, std::vector<std::vector<Symbol>>> productions;
+    std::string start_symbol;
+    explicit CFG_Reader(std::string &inputFilePath);
+private:
+    void insert_new_definition(std::string &rule_def, int line_number);
+    std::vector<std::vector<Symbol>> parse_production_rules(std::string &RHS, int line_number);
+    std::vector<Symbol> parse_rule(std::string &rule, int line_number);
 };
